1b9f40d55927ac82902fcf3c00d46d8d2fbd1e4e
vmerkle8331 <89599454+vmerkle8331@users.noreply.github.com>
2024-03-28 19:42:35 -0600

    Add files via upload

---
+++ Challenge3.1/Challenge3.1/Challenge3.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Challenge3._1
+{
+    partial class Challenge3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Challenge3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 390);
+            this.Name = "Challenge3";
+            this.Text = "Challenge ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Challenge3.1/Challenge3.1/Challenge3.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Challenge3._1
+{
+    public partial class Challenge3 : Form
+    {
+
+        Button[,] theGrid = new Button[10, 10];
+        Random random = new Random();
+
+        public Challenge3()
+        {
+            InitializeComponent();
+            InstantiateButtons();
+            CheckerFill(Color.Red, Color.Black);
+        }
+
+        public void InstantiateButtons()
+        {
+            Size btnSize = new Size(24, 24);
+
+
+            for (int row = 0; row < theGrid.GetLength(0); row++)
+            {
+                for (int col = 0; col < theGrid.GetLength(1); col++)
+                {
+                    theGrid[row, col] = new Button();
+                    theGrid[row, col].Size = btnSize;
+                    theGrid[row, col].Location = new Point(col * btnSize.Width, row * btnSize.Height);
+                    theGrid[row, col].Click += ButtonClickedHandler;
+                    this.Controls.Add(theGrid[row, col]);
+
+                }
+            }
+
+            this.Refresh();
+        }
+
+        public void CheckerFill(Color color1, Color color2)
+        {
+            for (int row = 0; row < theGrid.GetLength(0); row++)
+            {
+                for (int col = 0; col < theGrid.GetLength(1); col++)
+                {
+                    theGrid[row, col].BackColor = ((row + col) % 2 == 0) ? color1 : color2;
+                }
+            }
+        }
+
+        public void ButtonClickedHandler(object sender, EventArgs e)
+        {
+            Color oldColor = ((Button)sender).BackColor;
+            Color newColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+
+            foreach(Button button in theGrid)
+            {
+                if (button.BackColor == oldColor)
+                {
+                    button.BackColor = newColor;
+                }
+            }
+
+        }
+
+    }
+}
